3e53be0369c6ee60b18573ac7e27a9c3351d39bb
pastak <pasta0915@gmail.com>
2015-07-21 20:53:39 +0900

    ignore upload error

--- deb.sh
+++ deb.sh
@@ -6,5 +6,7 @@ VERSION=1.0
 tar czvf ../${PACKAGE}_${VERSION}.orig.tar.gz src icons
 debuild -us -uc -b || exit 1
 
-package_cloud push gyazo/gyazo-for-linux/ubuntu/trusty ../gyazo*all.deb
-package_cloud push gyazo/gyazo-for-linux/ubuntu/wheezy ../gyazo*all.deb
+# push to packagecloud.io
+# ignore error when version is not increamented
+package_cloud push gyazo/gyazo-for-linux/ubuntu/trusty ../gyazo*all.deb || true
+package_cloud push gyazo/gyazo-for-linux/ubuntu/wheezy ../gyazo*all.deb || true
