954d67c0d9885d3252f09540626bad7387059436
pastak <pasta0915@gmail.com>
2015-07-22 00:12:15 +0900

    separate build script and publish commands

--- deb.sh
+++ deb.sh
@@ -5,8 +5,3 @@ VERSION=1.0
 
 tar czvf ../${PACKAGE}_${VERSION}.orig.tar.gz src icons
 debuild -us -uc -b || exit 1
-
-# push to packagecloud.io
-# ignore error when version is not increamented
-package_cloud push gyazo/gyazo-for-linux/ubuntu/trusty ../gyazo*all.deb || true
-package_cloud push gyazo/gyazo-for-linux/ubuntu/wheezy ../gyazo*all.deb || true
