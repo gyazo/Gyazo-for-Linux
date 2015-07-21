a5294662c2b8c2bf382bee167d77e1e7ae02e471
pastak <pasta0915@gmail.com>
2015-07-21 20:37:38 +0900

    push in deb.sh

--- deb.sh
+++ deb.sh
@@ -5,3 +5,6 @@ VERSION=1.0
 
 tar czvf ../${PACKAGE}_${VERSION}.orig.tar.gz src icons
 debuild -us -uc -b || exit 1
+
+package_cloud push gyazo/gyazo-for-linux/ubuntu/trusty ../gyazo*all.deb
+package_cloud push gyazo/gyazo-for-linux/ubuntu/wheezy ../gyazo*all.deb
