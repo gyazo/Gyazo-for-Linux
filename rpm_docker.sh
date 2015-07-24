f1c30c09ba26c674d3c1d4c722cec0291a256c6c
pastak <pasta0915@gmail.com>
2015-07-25 03:33:23 +0900

    call package_cloud push in Docker

--- rpm_docker.sh
+++ rpm_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo service docker start
-docker build -t centos:latest .
-docker cp `docker ps -l -q`:/root/rpmbuild/RPMS/x86_64/${PACKAGE}-${VERSION}-1.x86_64.rpm ./
-package_cloud push gyazo/gyazo-for-linux/el/6 ./gyazo*.rpm || true
-package_cloud push gyazo/gyazo-for-linux/el/7 ./gyazo*.rpm || true
+docker build -t gyazo/build:latest .
+docker run -e PACKAGECLOUD_TOKEN=$PACKAGECLOUD_TOKEN gyazo/build package_cloud push gyazo/gyazo-for-linux/el/6 /root/rpmbuild/RPMS/x86_64/$PACKAGE-$VERSION-1.x86_64.rpm || true
+docker run -e PACKAGECLOUD_TOKEN=$PACKAGECLOUD_TOKEN gyazo/build package_cloud push gyazo/gyazo-for-linux/el/7 /root/rpmbuild/RPMS/x86_64/$PACKAGE-$VERSION-1.x86_64.rpm || true
