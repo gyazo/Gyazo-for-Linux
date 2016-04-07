2ca855a0f1d348d6e88f2fccda955bbe735b9fc1
drobune <s.yuichi227@gmail.com>
2016-04-07 18:21:59 +0900

    Refactoring gyazo ruby script
    Generate GyazoClient class.The reasons is next.
    
    - Reduce code size
    - Put codes of environment variables in one

--- src/gyazo.rb
+++ src/gyazo.rb
@@ -1,71 +1,42 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
 require 'open3'
-require 'openssl'
 require 'json'
 require 'yaml'
+require_relative 'gyazo_client'
 
 # setting
-configfile = "#{ENV['HOME']}/.gyazo.config.yml"
-config = {}
-if File.exist?(configfile) then
-  config = YAML.load_file(configfile)
-end
+config_file = "#{ENV['HOME']}/.gyazo.config.yml"
+opts = {}
+opts = YAML.load_file(config_file) if File.exist?(config_file)
 
-browser_cmd = config['browser_cmd'] || 'xdg-open'
-clipboard_cmd = config['clipboard_cmd'] || 'xclip'
-clipboard_opt = config['clipboard_opt'] || '-sel clip'
-host = config['host'] || 'upload.gyazo.com'
-cgi = config['cgi'] || '/upload.cgi'
-ua = config['ua'] || 'Gyazo/1.2'
-http_port = config['http_port'] || 443
-use_ssl = config['use_ssl'] == nil ? 'true' : config['use_ssl']
+gyazo_client = GyazoClient.new(opts)
 
-# get id
-idfile = ENV['HOME'] + "/.gyazo.id"
-
-id = ''
-if File.exist?(idfile) then
-  id = File.read(idfile).chomp
-end
-
-# get active window name
-
+# collect metadata
 active_window_id = `xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)" | cut -d ' ' -f 5`.chomp
 out, err, status = Open3.capture3 "xwininfo -id #{active_window_id} | grep \"xwininfo: Window id: \"|sed \"s/xwininfo: Window id: #{active_window_id}//\""
 active_window_name = out.chomp
 out, err, status = Open3.capture3 "xprop -id #{active_window_id} | grep \"_NET_WM_PID(CARDINAL)\" | sed s/_NET_WM_PID\\(CARDINAL\\)\\ =\\ //"
-
 pid = out.chomp
-
 application_name = `ps -p #{pid} -o comm=`.chomp
+
 # capture png file
-tmpfile = "/tmp/image_upload#{$$}.png"
-imagefile = ARGV[0]
-
-if imagefile && File.exist?(imagefile) then
-  system "convert '#{imagefile}' '#{tmpfile}'"
+temp = "/tmp/image_upload#{$$}.png"
+image_file = ARGV[0]
+if image_file && File.exist?(image_file)
+  system "convert #{image_file} #{temp}"
 else
-  command = (File.exist?(configfile) && YAML.load_file(configfile)['command']) || 'import'
-  system "#{command} '#{tmpfile}'"
+  system "#{gyazo_client.capture_cmd} #{temp}"
 end
 
-if !File.exist?(tmpfile) then
-  exit
-end
-
-imagedata = File.read(tmpfile)
-File.delete(tmpfile)
+fail 'No image data' unless File.exist?(temp)
+image_data = File.read(temp)
+File.delete(temp)
 
 xuri = ""
 if application_name =~ /(chrom(ium|e)|firefox|iceweasel)/
   xuri = `xdotool windowfocus #{active_window_id}; xdotool key "ctrl+l"; xdotool key "ctrl+c"; xclip -o`
 end
-
-
-# upload
-boundary = '----BOUNDARYBOUNDARY----'
 
 metadata = JSON.generate({
   app: active_window_name,
@@ -74,57 +45,4 @@ metadata = JSON.generate({
   note: "#{active_window_name}\n#{xuri}"
 })
 
-data = <<EOF
---#{boundary}\r
-content-disposition: form-data; name="metadata"\r
-\r
-#{metadata}\r
---#{boundary}\r
-content-disposition: form-data; name="id"\r
-\r
-#{id}\r
---#{boundary}\r
-content-disposition: form-data; name="imagedata"; filename="gyazo.com"\r
-\r
-#{imagedata}\r
---#{boundary}--\r
-EOF
-
-header ={
-  'Content-Length' => data.length.to_s,
-  'Content-type' => "multipart/form-data; boundary=#{boundary}",
-  'User-Agent' => ua
-}
-
-env = ENV['http_proxy']
-if env then
-  uri = URI(env)
-  proxy_host, proxy_port = uri.host, uri.port
-else
-  proxy_host, proxy_port = nil, nil
-end
-https = Net::HTTP::Proxy(proxy_host, proxy_port).new(host,http_port)
-https.use_ssl = use_ssl
-https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-https.verify_depth = 5
-https.start{
-  res = https.post(cgi,data,header)
-  url = res.response.body
-  puts url
-  if system "which #{clipboard_cmd} >/dev/null 2>&1" then
-    system "echo -n '#{url}' | #{clipboard_cmd} #{clipboard_opt}"
-  end
-  system "#{browser_cmd} '#{url}'"
-
-  # save id
-  newid = res.response['X-Gyazo-Id']
-  if newid and newid != "" then
-    if !File.exist?(File.dirname(idfile)) then
-      Dir.mkdir(File.dirname(idfile))
-    end
-    if File.exist?(idfile) then
-      File.rename(idfile, idfile+Time.new.strftime("_%Y%m%d%H%M%S.bak"))
-    end
-    File.open(idfile,"w").print(newid)
-  end
-}
+gyazo_client.upload(image_data, metadata)
---
+++ src/gyazo_client.rb
@@ -0,0 +1,89 @@
+require 'net/http'
+require 'open3'
+require 'openssl'
+
+class GyazoClient
+  ID_FILE = ENV['HOME'] + "/.gyazotest.id"
+
+  def initialize(opts = {})
+    default_opts = {
+      capture_cmd:  'import',
+      browser_cmd:   'xdg-open',
+      clipboard_cmd: 'xclip',
+      host:          'upload.gyazo.com',
+      cgi:           '/upload.cgi',
+      ua:            'Gyazo/1.2',
+      port:          443,
+      use_ssl:       true
+    }
+
+    opts = default_opts.merge(opts)
+
+    opts.each do |key, value|
+      self.class.class_eval { attr_accessor "#{key}" }
+      self.instance_variable_set "@#{key}", value
+    end
+  end
+
+  def upload(image_data, metadata)
+    boundary = '----BOUNDARYBOUNDARY----'
+
+    data = <<EOF
+--#{boundary}\r
+content-disposition: form-data; name="metadata"\r
+\r
+#{metadata}\r
+--#{boundary}\r
+content-disposition: form-data; name="id"\r
+\r
+#{gyazo_id}\r
+--#{boundary}\r
+content-disposition: form-data; name="imagedata"; filename="gyazo.com"\r
+\r
+#{image_data}\r
+--#{boundary}--\r
+EOF
+
+    header ={
+      'Content-Length' => data.length.to_s,
+      'Content-type' => "multipart/form-data; boundary=#{boundary}",
+      'User-Agent' => @ua
+    }
+
+    if @http_proxy
+      proxy = URI.parse(@http_proxy)
+      @proxy_host, @proxy_port = proxy.host, proxy.port
+    end
+
+    https = Net::HTTP::Proxy(@proxy_host, @proxy_port).new(@host, @port)
+    https.use_ssl = @use_ssl
+    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    https.verify_depth = 5
+
+    res = https.post(@cgi, data, header)
+    gyazo_url = res.response.body
+
+    puts gyazo_url
+
+    if system "which #{@clipboard_cmd} >/dev/null 2>&1"
+      system "echo -n '#{gyazo_url}' | #{@clipboard_cmd}"
+    end
+    system "#{browser_cmd} '#{gyazo_url}'"
+
+    new_id = res.response['X-Gyazo-Id']
+    save_id(new_id) if new_id && new_id != ''
+  end
+
+  private
+  def gyazo_id
+    return '' unless File.exist?(ID_FILE)
+    id = File.read(ID_FILE).chomp
+  end
+
+  def save_id(new_id)
+    if gyazo_id != ''
+      File.rename(ID_FILE, ID_FILE+Time.new.strftime("_%Y%m%d%H%M%S.bak"))
+    end
+    File.open(ID_FILE,"w").print(new_id)
+  end
+end
