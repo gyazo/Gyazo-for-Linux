15514c773573012f48a0feed38c383391a0b3ad3
drobune <s.yuichi227@gmail.com>
2016-04-11 20:43:12 +0900

    Add bundle exec to package_cloud command

--- rpm_release.rb
+++ rpm_release.rb
@@ -10,6 +10,6 @@ json['rpm'].each do |item|
   os = item['index_name']
   item['versions'].each do |version|
     target = "#{os}/#{version['index_name']}"
-    `package_cloud push gyazo/gyazo-for-linux/#{target} /root/rpmbuild/RPMS/x86_64/gyazo-#{VERSION}-1.x86_64.rpm`
+    `bundle exec package_cloud push gyazo/gyazo-for-linux/#{target} /root/rpmbuild/RPMS/x86_64/gyazo-#{VERSION}-1.x86_64.rpm`
   end
 end
